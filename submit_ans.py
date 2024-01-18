b66d0638e382a1f220184d17342d5407fefaa098
Andrew Gu <67293785+MonkeyKing-1@users.noreply.github.com>
2024-01-18 17:02:52 -0500

    feat: looping client

--- submit_ans.py
+++ submit_ans.py
@@ -24,17 +24,18 @@ contract_address = "0x6229FD3198f672D0d971fb9b6929d19A4650D951"
 # Create smart contract instance
 contract = web3.eth.contract(address=contract_address, abi=abi)
 # initialize the chain id, we need it to build the transaction for replay protection
-Chain_id = web3.eth.chain_id
-start_nonce = web3.eth.get_transaction_count(sender_address)
+while(True):
+    start_nonce = web3.eth.get_transaction_count(sender_address)
 
-print("current quotient is:", contract.functions.cur_quotient().call())
-ans = int(input("enter a prime divisor: "))
+    print("current quotient is:", contract.functions.cur_quotient().call())
+    ans = int(input("enter a prime divisor: "))
 
-call_function = contract.functions.check_sol(ans).build_transaction({"from": sender_address, "value": 10 ** 17, "nonce": start_nonce})
-# Sign transaction
-signed_tx = web3.eth.account.sign_transaction(call_function, private_key=private_key)
-# Send transaction
-send_tx = web3.eth.send_raw_transaction(signed_tx.rawTransaction)
-# Wait for transaction receipt
-tx_receipt = web3.eth.wait_for_transaction_receipt(send_tx)
-print(tx_receipt.status)
+    call_function = contract.functions.check_sol(ans).build_transaction({"from": sender_address, "value": 10 ** 17, "nonce": start_nonce})
+    # Sign transaction
+    signed_tx = web3.eth.account.sign_transaction(call_function, private_key=private_key)
+    # Send transaction
+    send_tx = web3.eth.send_raw_transaction(signed_tx.rawTransaction)
+    # Wait for transaction receipt
+    tx_receipt = web3.eth.wait_for_transaction_receipt(send_tx)
+    print(tx_receipt.status)
+    time.sleep(1)
